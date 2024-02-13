1029da0a41cedbea299d10e7d469831b506b0388
rishav6611 <hajariwalarishav@gmail.com>
2024-02-13 22:41:54 +0530

    fixes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,3 +1,27 @@
+// const JWT = require("jsonwebtoken");
+
+// const protect = async (req, res, next) => {
+//   var token;
+//   try {
+//     if (req.headers.authorization) {
+//       token = req.headers.authorization;
+
+//       const decoded = JWT.verify(token, process.env.JWT_SECREAT);
+//       req.body.userId = decoded.id;
+//       next();
+//     } else {
+//       res.status(400).json({
+//         error: "authorization failed",
+//       });
+//     }
+//     // throw new error();
+//   } catch (error) {
+//     return res.status(500).json({ error: error });
+//   }
+// };
+
+// module.exports = protect;
+
 const express = require('express');
 const { loginController, registerController } = require('../controllers/AuthController');
 const protect = require('../middlewares/authMiddlewares');
@@ -8,6 +32,7 @@ const router = express.Router();
 
 //routes
 //login
+//hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
 router.post('/login', loginController);
 
 //register
